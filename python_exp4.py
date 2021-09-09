128272a4948e01e029d4e9730c73e1c10d9128db
rajatrc1705 <rajatrc1705@gmail.com>
2021-09-09 15:46:14 +0530

    python program for jenkins build

---
+++ python_exp4.py
@@ -0,0 +1,12 @@
+
+def addition(x, y):
+
+    sum = x+y
+    return sum
+
+
+summation = addition(3, 4)
+
+print("====================================================")
+print(f"The summation of 3 and 4 comes out to be {summation}")
+print("====================================================")
